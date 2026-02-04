baace465b21880dd3c1e3f5d87acb378b1c9f36e
caohongming <hcao0907@outlook.com>
2026-02-04 06:53:15 +0000

    蒙东负荷训练脚本

--- devlog.md
+++ devlog.md
@@ -8,11 +8,22 @@
 cd TSF_architecture/
 bash scripts/all_models/etth1.sh
 bash scripts/all_models/electricity.sh
+bash scripts/蒙东负荷/mongolia.sh
 ```
 
 ### 参数调整
 
-### Jasn 29 
+### Feb 4
+- 新增 mongolia.sh
+    - seq_len (3x24) 72 三天
+    - pred_len (7x24) 168 七天
+    - patch 调成 12 匹配窗口大小
+    - batch_size 32
+    - train_ratio 0.6
+    - features S 单一对单一
+    - 用 --inverse 还原数据后再计算指标
+
+### Jan 29 
 - electricity.sh
     - 历史 256 预测 48 步
     - features MS 预测最后一列
---
+++ scripts/蒙东负荷/mongolia.sh
@@ -0,0 +1,80 @@
+root_path_name=./dataset/
+data_path_name=mongolia.csv
+model_id_name=mongolia
+data_name=custom
+
+# seq_len=104
+# model_name=PatchTST
+# model_name=Transformer
+# model_name=Transformer_patch
+
+gpu_num=0
+
+random_seed=2021
+
+
+# for model_name in Encoder Encoder_overall Encoder_zeros Masked_encoder Prefix_decoder Decoder Transformer
+# for model_name in PatchTST Encoder Encoder_overall Encoder_zeros Masked_encoder Prefix_decoder Decoder Transformer Encoder_zeros_flatten Masked_encoder_flatten Double_decoder Double_encoder
+for model_name in Encoder_overall
+do
+if [[ "$model_name" =~ "Encoder" || "$model_name" =~ "encoder" ]]; then
+    e_layers=6
+    d_layers=0
+elif [[ "$model_name" =~ "Decoder" || "$model_name" =~ "decoder" || "$model_name" =~ "PatchTST" ]]; then
+    e_layers=0
+    d_layers=6
+elif [[ "$model_name" =~ "Transformer" ]]; then
+    e_layers=3
+    d_layers=3
+fi
+# for norm in layer batch
+for norm in layer
+do
+for seq_len in 72
+do
+# for pred_len in 96
+for pred_len in 168
+do
+    if [ ! -d './script_outputs/' ]; then
+        mkdir './script_outputs/'
+    fi
+    if [ ! -d './script_outputs/'$model_id_name'_'$seq_len'_'$pred_len'/' ]; then
+        mkdir './script_outputs/'$model_id_name'_'$seq_len'_'$pred_len'/'
+    fi
+    
+    python -u run_longExp.py \
+      --random_seed $random_seed \
+      --is_training 1 \
+      --root_path $root_path_name \
+      --data_path $data_path_name \
+      --model_id $model_id_name'_'$seq_len'_'$pred_len \
+      --model $model_name \
+      --data $data_name \
+      --features S \
+      --target 蒙东总供电 \
+      --seq_len $seq_len \
+      --pred_len $pred_len \
+      --e_layers $e_layers \
+      --d_layers $d_layers \
+      --factor 3 \
+      --enc_in 1 \
+      --dec_in 1 \
+      --c_out 1 \
+      --d_model 512 \
+      --des 'Exp' \
+      --itr 1 \
+      --inverse \
+      --train_ratio 0.6 \
+      --train_epochs 20 \
+      --patience 3 \
+      --patch_len 12 \
+      --stride 12 \
+      --gpu $gpu_num \
+      --batch_size 32 \
+      --run_train --run_test \
+      --norm $norm \
+      > './script_outputs/'$model_id_name'_'$seq_len'_'$pred_len'/'$model_name'_'$norm'norm'.log
+done
+done
+done
+done
